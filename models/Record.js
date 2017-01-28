bf042dfd4eb4c2c0290823d5db05ca67bf2f6dd2
Allen Cheng <allen12@umd.edu>
2017-01-28 14:36:18 -0500

    Add new models for app

---
+++ models/Record.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const path = require('path');
+
+const schemaService = require(path.resolve('./services/schema.server.service'));
+const GetterSchema = schemaService.GetterSchema;
+
+const util = require(path.resolve('./services/util.server.service'));
+
+
+/**
+ * Record Schema
+ */
+
+let RecordSchema = new GetterSchema({
+	user: {
+		type: String,
+		ref: 'User',
+		required: 'A valid user is required'
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+		get: util.dateParse
+	},
+	food: [{
+		name: {
+			type: String,
+			trim: true,
+			required: 'Food name is required'
+		},
+		quantity: {
+			type: Number,
+			required: 'Food quantity is required'
+		},
+		measurement: {
+			type: String,
+			trim: true,
+			required: 'Food measurement is required'
+		},
+		calories: {
+			type: Number,
+			required: 'Food calories is required'
+		},
+		time: {
+			type: Date,
+			default: Date.now,
+			get: util.dateParse
+		}
+	}],
+	steps: {
+		type: Number,
+		default: 0
+	},
+	sleep: {
+		type: Number,
+		default: 0
+	},
+	heartRate: {
+		type: Number
+	}
+});
+
+/**
+ * Index declarations
+ */
+
+// Text-search index
+RecordSchema.index({
+	user: 'text',
+	'food.name': 'text',
+});
+
+const Record = mongoose.model('Record', RecordSchema);
+module.exports = Record;
--- models/User.js
+++ models/User.js
@@ -15,16 +15,14 @@ const util = require(path.resolve('./services/util.server.service'));
  */
 
 let UserSchema = new GetterSchema({
-  firstName: {
-  	type: String,
-    trim: true,
-    // required: 'First name is required'
-  },
-  lastName: {
-  	type: String,
-    trim: true,
-    // required: 'Last name is required'
-  },
+	firstName: {
+		type: String,
+		trim: true
+	},
+	lastName: {
+		type: String,
+		trim: true
+	},
 	username: {
 		type: String,
 		trim: true,
@@ -37,35 +35,20 @@ let UserSchema = new GetterSchema({
 		required: 'Email is required',
 		match: [/.+\@.+\..+/, 'A valid email address is required']
 	},
-	roles: {
-		type: {
-			user: {
-				type: Boolean,
-				default: true
-			},
-			admin: {
-				type: Boolean,
-				default: false
-			},
-			superuser: {
-				type: Boolean,
-				default: false
-			}
+	health: {
+		weight: {
+			type: Number
+		},
+		height: {
+			type: Number
+		},
+		gender: {
+			type: String,
+			trim: true
+		},
+		age: {
+			type: Number
 		}
-	},
-	resume: {
-    	type: String
-	},
-	major: {
-    	type: String,
-		// required: 'Major is required',
-		trim: true
-	},
-	// class standing (e.g. Freshman, Sophomore, etc.)
-	class: {
-  	type: String,
-		// required: 'Class standing is required',
-		trim: true
 	},
 	updated: {
 		type: Date,
@@ -93,52 +76,7 @@ UserSchema.index({
   lastName: 'text',
   email: 'text',
   username: 'text',
-  'roles.type.user': 'boolean',
-  'roles.type.admin': 'boolean',
-  'roles.type.superuser': 'boolean',
-  major: 'text',
-  class: 'text'
 });
-
-
-/**
- * Password hash middleware.
- */
-UserSchema.pre('save', function save(next) {
-  const user = this;
-  if (!user.isModified('password')) { return next(); }
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) { return next(err); }
-    bcrypt.hash(user.password, salt, null, (err, hash) => {
-      if (err) { return next(err); }
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-/**
- * Helper method for validating user's password.
- */
-UserSchema.methods.comparePassword = function comparePassword(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    cb(err, isMatch);
-  });
-};
-
-/**
- * Helper method for getting user's gravatar.
- */
-UserSchema.methods.gravatar = function gravatar(size) {
-  if (!size) {
-    size = 200;
-  }
-  if (!this.email) {
-    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
-  }
-  const md5 = crypto.createHash('md5').update(this.email).digest('hex');
-  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
-};
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
